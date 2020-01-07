f20644674b8dd537c6c44d302f028225bf21a7f2
daur <daur-lion@mail.ru>
2020-01-07 08:43:41 +0600

    Category v1 crud

--- app/Http/Controllers/Web/v1/Admin/CategoryController.php
+++ app/Http/Controllers/Web/v1/Admin/CategoryController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Web\v1\Admin;
 use App\Category;
 use App\Http\Controllers\Controller;
 use App\Services\v1\CategoryService;
+use Illuminate\Http\Request;
+use Session;
+
+
 
 class CategoryController extends Controller
 {
@@ -21,5 +25,86 @@ class CategoryController extends Controller
     {
         $categories = $this->categoryService->findAll();
         return view('admin/category/index', compact('categories'));
+    }
+
+
+    public function create()
+    {   $categories = Category::all();
+        return view('admin.categories.create')
+            ->with('categories',$categories);
+    }
+
+
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+           'name'=> 'required',
+            'type'=>'required',
+        ]);
+
+        $category = Category::create([
+            'name'=>$request->name,
+            'type'=>$request->type,
+            'parent_category_id'=>$request->parent_category_id
+
+        ]);
+
+        $category->save();
+
+        Session::flash('success','Category created successfully');
+
+        return redirect()->route('categories.index');
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+
+    public function edit($id)
+    {
+        $categories = Category::all();
+        return view('admin.categories.edit')
+            ->with('categories',$categories)
+            ->with('category', Category::findOrFail($id));
+
+    }
+
+    public function update(Request $request,$id)
+    {
+        $this->validate($request,[
+            'name'=> 'required',
+            'type'=>'required',
+        ]);
+
+        Category::findOrFail($id)
+            ->update([
+                'name'=>$request->name,
+                'type'=>$request->type,
+                'parent_category_id'=>$request->parent_category_id
+            ]);
+//        $category->name = $request->name;
+//        $category->type = $request->type;
+//        $category->parent_category_id = $request->parent_category_id;
+//        $category->save();
+
+        Session::flash('success','Category updated successfully');
+
+        return redirect()->route('categories.index');
+
+    }
+
+    public function destroy($id)
+    {
+        $child_category_ids = Category::where('parent_category_id', $id)->get('id');
+
+        Category::destroy($child_category_ids);
+
+        Category::destroy($id);
+
+        Session::flash('success','Category deleted successfully');
+
+        return redirect()->route('categories.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,10 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['namespace' => 'Web\v1\Admin'], function () {
 
-    Route::get('/test', 'CategoryController@index')->name('category.index');
-
+    Route::get('/categories', 'CategoryController@index')->name('categories.index');
+    Route::get('/category/create','CategoryController@create')->name('category.create');
+    Route::post('/category/store','CategoryController@store')->name('category.store');
+    Route::get('/category/edit/{id}','CategoryController@edit')->name('category.edit');
+    Route::post('/category/update/{id}','CategoryController@update')->name('category.update');
+    Route::delete('/category/delete/{id}','CategoryController@destroy')->name('category.destroy');
 });
