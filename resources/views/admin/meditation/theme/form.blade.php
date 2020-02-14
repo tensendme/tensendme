568f4beb3b795881cba4fb5a636a49296e88a55f
unknown_haribda <bekza.bekmuratov@gmail.com>
2020-02-14 13:47:18 +0600

    Meditation front view

--- resources/views/admin/meditation/theme/form.blade.php
+++ resources/views/admin/meditation/theme/form.blade.php
@@ -10,24 +10,24 @@
         <label class="form-control-plaintext" for="title">Пожалуйста введите название медитации</label>
     </div>
 </div>
-<?php $i=0;?>
 @foreach($audioTypes as $audioType)
     <p>{{$audioType->name}}</p>
     <div class="col-md-6">
         <div class="form-group">
-            @if(!empty($theme->audios[$i]))
-            @if($theme->audios[$i]->audio_language_id == $audioType->id)
-                <audio src="{{asset($theme->audios[$i]->audio_path)}}" controls>
-                    Не поддерживается
-                </audio>
-            @endif
+            @if(!empty($theme->audios))
+            @foreach($theme->audios as $audio)
+                @if($audio->audio_language_id == $audioType->id)
+                    <audio src="{{asset($audio->audio_path)}}" controls>
+                        Не поддерживается
+                    </audio>
+                    @endif
+                @endforeach
             @else Не добавлено
             @endif
             <input class="form-control" type="file" name="audio[{{$audioType->id}}]" id="audio{{$audioType->id}}" accept=".mp3">
             <label class="form-control-plaintext" for="audio{{$audioType->id}}">Пожалуйста выберите аудио</label>
         </div>
     </div>
-    <?php $i++?>
 @endforeach
 <div class="form-group col-md-12 text-right">
     <button class="mb-2 btn btn-primary mr-1" type="submit">Сохранить
