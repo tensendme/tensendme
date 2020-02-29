7d427f9977ac442379d93c12198efd7ab0d71e1b
air <asilkhan-al@mail.ru>
2020-03-01 02:34:47 +0600

    Payment page is already done

--- resources/views/cryptogram.blade.php
+++ resources/views/cryptogram.blade.php
@@ -28,7 +28,7 @@
             font-style: normal;
             font-weight: 400;
             font-display: swap;
-            src: local('Montserrat Regular'), local('Montserrat-Regular'), url({{ storage_path('fonts/Montserrat-Regular.ttf') }}) format("truetype");;
+            src: local('Montserrat Regular'), local('Montserrat-Regular'), url({{ asset('fonts/Montserrat-Regular.ttf') }}) format("truetype");;
             unicode-range: U+0400-045F, U+0490-0491, U+04B0-04B1, U+2116;
         }
 
@@ -38,7 +38,7 @@
             font-style: normal;
             font-weight: 400;
             font-display: swap;
-            src: local('Montserrat Regular'), local('Montserrat-Regular'), url({{ storage_path('fonts/Montserrat-Regular.ttf') }}) format("truetype");
+            src: local('Montserrat Regular'), local('Montserrat-Regular'), url({{ asset('fonts/Montserrat-Regular.ttf') }}) format("truetype");
             unicode-range: U+0102-0103, U+0110-0111, U+0128-0129, U+0168-0169, U+01A0-01A1, U+01AF-01B0, U+1EA0-1EF9, U+20AB;
         }
 
@@ -48,7 +48,7 @@
             font-style: normal;
             font-weight: 400;
             font-display: swap;
-            src: local('Montserrat Regular'), local('Montserrat-Regular'), url({{ storage_path('fonts/Montserrat-Regular.ttf') }}) format("truetype");
+            src: local('Montserrat Regular'), local('Montserrat-Regular'), url({{ asset('fonts/Montserrat-Regular.ttf') }}) format("truetype");
             unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
         }
 
@@ -58,7 +58,7 @@
             font-style: normal;
             font-weight: 400;
             font-display: swap;
-            src: local('Montserrat Regular'), local('Montserrat-Regular'), url({{ storage_path('fonts/Montserrat-Regular.ttf') }}) format("truetype");
+            src: local('Montserrat Regular'), local('Montserrat-Regular'), url({{ asset('fonts/Montserrat-Regular.ttf') }}) format("truetype");
             unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
         }
 
@@ -68,7 +68,7 @@
             font-style: normal;
             font-weight: 600;
             font-display: swap;
-            src: local('Montserrat SemiBold'), local('Montserrat-SemiBold'), url({{ storage_path('fonts/Montserrat-SemiBold.ttf') }}) format("truetype");
+            src: local('Montserrat SemiBold'), local('Montserrat-SemiBold'), url({{ asset('fonts/Montserrat-SemiBold.ttf') }}) format("truetype");
             unicode-range: U+0460-052F, U+1C80-1C88, U+20B4, U+2DE0-2DFF, U+A640-A69F, U+FE2E-FE2F;
         }
 
@@ -78,7 +78,7 @@
             font-style: normal;
             font-weight: 600;
             font-display: swap;
-            src: local('Montserrat SemiBold'), local('Montserrat-SemiBold'), url({{ storage_path('fonts/Montserrat-SemiBold.ttf') }}) format("truetype");
+            src: local('Montserrat SemiBold'), local('Montserrat-SemiBold'), url({{ asset('fonts/Montserrat-SemiBold.ttf') }}) format("truetype");
             unicode-range: U+0400-045F, U+0490-0491, U+04B0-04B1, U+2116;
         }
 
@@ -88,7 +88,7 @@
             font-style: normal;
             font-weight: 600;
             font-display: swap;
-            src: local('Montserrat SemiBold'), local('Montserrat-SemiBold'), url({{ storage_path('fonts/Montserrat-SemiBold.ttf') }}) format("truetype");
+            src: local('Montserrat SemiBold'), local('Montserrat-SemiBold'), url({{ asset('fonts/Montserrat-SemiBold.ttf') }}) format("truetype");
             unicode-range: U+0102-0103, U+0110-0111, U+0128-0129, U+0168-0169, U+01A0-01A1, U+01AF-01B0, U+1EA0-1EF9, U+20AB;
         }
 
@@ -98,7 +98,7 @@
             font-style: normal;
             font-weight: 600;
             font-display: swap;
-            src: local('Montserrat SemiBold'), local('Montserrat-SemiBold'), url({{ storage_path('fonts/Montserrat-SemiBold.ttf') }}) format("truetype");
+            src: local('Montserrat SemiBold'), local('Montserrat-SemiBold'), url({{ asset('fonts/Montserrat-SemiBold.ttf') }}) format("truetype");
             unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
         }
 
@@ -108,7 +108,7 @@
             font-style: normal;
             font-weight: 600;
             font-display: swap;
-            src: local('Montserrat SemiBold'), local('Montserrat-SemiBold'), url({{ storage_path('fonts/Montserrat-SemiBold.ttf') }}) format("truetype");
+            src: local('Montserrat SemiBold'), local('Montserrat-SemiBold'), url({{ asset('fonts/Montserrat-SemiBold.ttf') }}) format("truetype");
             unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
         }
 
@@ -248,7 +248,7 @@
 <div class="container">
     <div class="first-row">
         <h4>
-            ТОО «Tensend»
+            ЖШС «Tensend»
         </h4>
         <p class="too-info">
             БИН: 190440000695
--- resources/views/pdf_view.blade.php
+++ resources/views/pdf_view.blade.php
@@ -11,7 +11,7 @@
             font-style: normal;
             font-weight: 400;
             font-display: swap;
-            src: local('Montserrat Regular'), local('Montserrat-Regular'), url({{ storage_path('fonts/Montserrat-Regular.ttf') }}) format("truetype");
+            src: local('Montserrat Regular'), local('Montserrat-Regular'), url({{ asset('fonts/Montserrat-Regular.ttf') }}) format("truetype");
             unicode-range: U+0460-052F, U+1C80-1C88, U+20B4, U+2DE0-2DFF, U+A640-A69F, U+FE2E-FE2F;
         }
 
@@ -21,7 +21,7 @@
             font-style: normal;
             font-weight: 400;
             font-display: swap;
-            src: local('Montserrat Regular'), local('Montserrat-Regular'), url({{ storage_path('fonts/Montserrat-Regular.ttf') }}) format("truetype");;
+            src: local('Montserrat Regular'), local('Montserrat-Regular'), url({{ asset('fonts/Montserrat-Regular.ttf') }}) format("truetype");;
             unicode-range: U+0400-045F, U+0490-0491, U+04B0-04B1, U+2116;
         }
 
@@ -31,7 +31,7 @@
             font-style: normal;
             font-weight: 400;
             font-display: swap;
-            src: local('Montserrat Regular'), local('Montserrat-Regular'), url({{ storage_path('fonts/Montserrat-Regular.ttf') }}) format("truetype");
+            src: local('Montserrat Regular'), local('Montserrat-Regular'), url({{ asset('fonts/Montserrat-Regular.ttf') }}) format("truetype");
             unicode-range: U+0102-0103, U+0110-0111, U+0128-0129, U+0168-0169, U+01A0-01A1, U+01AF-01B0, U+1EA0-1EF9, U+20AB;
         }
 
@@ -41,7 +41,7 @@
             font-style: normal;
             font-weight: 400;
             font-display: swap;
-            src: local('Montserrat Regular'), local('Montserrat-Regular'), url({{ storage_path('fonts/Montserrat-Regular.ttf') }}) format("truetype");
+            src: local('Montserrat Regular'), local('Montserrat-Regular'), url({{ asset('fonts/Montserrat-Regular.ttf') }}) format("truetype");
             unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
         }
 
@@ -51,7 +51,7 @@
             font-style: normal;
             font-weight: 400;
             font-display: swap;
-            src: local('Montserrat Regular'), local('Montserrat-Regular'), url({{ storage_path('fonts/Montserrat-Regular.ttf') }}) format("truetype");
+            src: local('Montserrat Regular'), local('Montserrat-Regular'), url({{ asset('fonts/Montserrat-Regular.ttf') }}) format("truetype");
             unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
         }
 
@@ -61,7 +61,7 @@
             font-style: normal;
             font-weight: 600;
             font-display: swap;
-            src: local('Montserrat SemiBold'), local('Montserrat-SemiBold'), url({{ storage_path('fonts/Montserrat-SemiBold.ttf') }}) format("truetype");
+            src: local('Montserrat SemiBold'), local('Montserrat-SemiBold'), url({{ asset('fonts/Montserrat-SemiBold.ttf') }}) format("truetype");
             unicode-range: U+0460-052F, U+1C80-1C88, U+20B4, U+2DE0-2DFF, U+A640-A69F, U+FE2E-FE2F;
         }
 
@@ -71,7 +71,7 @@
             font-style: normal;
             font-weight: 600;
             font-display: swap;
-            src: local('Montserrat SemiBold'), local('Montserrat-SemiBold'), url({{ storage_path('fonts/Montserrat-SemiBold.ttf') }}) format("truetype");
+            src: local('Montserrat SemiBold'), local('Montserrat-SemiBold'), url({{ asset('fonts/Montserrat-SemiBold.ttf') }}) format("truetype");
             unicode-range: U+0400-045F, U+0490-0491, U+04B0-04B1, U+2116;
         }
 
@@ -81,7 +81,7 @@
             font-style: normal;
             font-weight: 600;
             font-display: swap;
-            src: local('Montserrat SemiBold'), local('Montserrat-SemiBold'), url({{ storage_path('fonts/Montserrat-SemiBold.ttf') }}) format("truetype");
+            src: local('Montserrat SemiBold'), local('Montserrat-SemiBold'), url({{ asset('fonts/Montserrat-SemiBold.ttf') }}) format("truetype");
             unicode-range: U+0102-0103, U+0110-0111, U+0128-0129, U+0168-0169, U+01A0-01A1, U+01AF-01B0, U+1EA0-1EF9, U+20AB;
         }
 
@@ -91,7 +91,7 @@
             font-style: normal;
             font-weight: 600;
             font-display: swap;
-            src: local('Montserrat SemiBold'), local('Montserrat-SemiBold'), url({{ storage_path('fonts/Montserrat-SemiBold.ttf') }}) format("truetype");
+            src: local('Montserrat SemiBold'), local('Montserrat-SemiBold'), url({{ asset('fonts/Montserrat-SemiBold.ttf') }}) format("truetype");
             unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
         }
 
@@ -101,7 +101,7 @@
             font-style: normal;
             font-weight: 600;
             font-display: swap;
-            src: local('Montserrat SemiBold'), local('Montserrat-SemiBold'), url({{ storage_path('fonts/Montserrat-SemiBold.ttf') }}) format("truetype");
+            src: local('Montserrat SemiBold'), local('Montserrat-SemiBold'), url({{ asset('fonts/Montserrat-SemiBold.ttf') }}) format("truetype");
             unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
         }
 
