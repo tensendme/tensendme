ccf5e7238907c02a0b52e03e1c62e25dd1891949
daur <daur-lion@mail.ru>
2020-03-10 17:06:02 +0600

    service impl v3

--- app/Utils/PaymentUtil.php
+++ app/Utils/PaymentUtil.php
@@ -12,15 +12,18 @@ class PaymentUtil
     public const _TOKEN_CHARGE = 'https://api.cloudpayments.ru/payments/tokens/charge';
     public  const  _CARDS_CHARGE = 'https://api.cloudpayments.ru/payments/cards/charge';
     public const _REFUND_CONFIRM_URL = 'https://api.cloudpayments.ru/payments/confirm';
+    public const _CLOUDPAYMENTS_USERNAME = 'pk_5ca541f82448e11afb98b5c1a3ffa';
+    public const _CLOUDPAYMENTS_PASSWORD = 'cc06d6b7614d4834809dfdd755527f1a';
+
 
     public static function getUsername()
     {
-        return env('CLOUDPAYMENTS_USERNAME', 'pk_5ca541f82448e11afb98b5c1a3ffa');
+        return self::_CLOUDPAYMENTS_USERNAME;
     }
 
     public static function getPassword()
     {
-        return env('CLOUDPAYMENTS_PASSWORD', 'cc06d6b7614d4834809dfdd755527f1a');
+        return self::_CLOUDPAYMENTS_PASSWORD;
     }
 
     public static function getBase64EncodedAuthToken()
