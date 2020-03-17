8bc0ae24a4f63643a519b5e366657bd2ddc6774c
unknown_haribda <bekza.bekmuratov@gmail.com>
2020-03-18 02:29:11 +0600

    Access;

--- app/Services/v1/impl/MaterialServiceImpl.php
+++ app/Services/v1/impl/MaterialServiceImpl.php
@@ -14,8 +14,8 @@ class MaterialServiceImpl implements MaterialService
 {
     public function getMaterialById($id)
     {
-        $user = Auth::user();
-        $subscriptions = $user->activeSubscriptions();
+//        $user = Auth::user();
+//        $subscriptions = $user->activeSubscriptions();
         $material = CourseMaterial::where('id', $id)->with(['documents', 'course'])->first();
         if(!$material) throw new ApiServiceException(404, false, [
             'errors' => [
@@ -23,12 +23,12 @@ class MaterialServiceImpl implements MaterialService
             ],
             'errorCode' => ErrorCode::RESOURCE_NOT_FOUND
         ]);
-        if(!$subscriptions->exists() && !$material->free) throw new ApiServiceException(404, false, [
-            'errors' => [
-                'Вы можете открыть доступ к этому уроку купив подписку'
-            ],
-            'errorCode' => ErrorCode::ACCESS_DENIED
-        ]);
+//        if(!$subscriptions->exists() && !$material->free) throw new ApiServiceException(404, false, [
+//            'errors' => [
+//                'Вы можете открыть доступ к этому уроку купив подписку'
+//            ],
+//            'errorCode' => ErrorCode::ACCESS_DENIED
+//        ]);
         return $material;
     }
 
