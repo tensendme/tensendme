913cbf2e0e478c47a0f7ace056dddcec1f8969a3
air <asilkhan-al@mail.ru>
2020-03-31 15:15:42 +0600

    Mass push sending done but need to be tested correctly

---
+++ database/migrations/2020_03_31_151451_alter_table_content_column_in_pushes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTableContentColumnInPushesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pushes', function (Blueprint $table) {
+            $table->dropColumn('content');
+            $table->text('content')
+                ->nullable(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pushes', function (Blueprint $table) {
+            $table->dropColumn('content');
+            $table->string('content')
+                ->nullable(true);
+        });
+    }
+}
