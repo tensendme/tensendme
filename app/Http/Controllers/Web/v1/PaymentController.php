f9d9d5ceb99d45b022b3b8a4ef1a63f41fbf181f
air <asilkhan-al@mail.ru>
2020-04-14 00:31:45 +0600

    Device token nullable

--- app/Http/Controllers/Web/v1/PaymentController.php
+++ app/Http/Controllers/Web/v1/PaymentController.php
@@ -45,7 +45,9 @@ class PaymentController extends WebBaseController
 
     public function subscribe()
     {
-        $subscriptions = SubscriptionType::where('price', '!=', 0)->get();
+        $subscriptions = SubscriptionType::where('is_visible', true)
+            ->where('price', '!=', 0)
+            ->get();
         $countries = Country::all();
         return view('subscription', compact('subscriptions', 'countries'));
     }
