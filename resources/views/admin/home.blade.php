e3c83502903b50ba937f92cb2a275f6683aed82b
air <asilkhan-al@mail.ru>
2020-04-22 03:34:26 +0600

    Responsiveness added

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -81,7 +81,7 @@
                             <h6 class="stats-small__value count my-3">{{$referralNumber ?? 0}}</h6>
                             <p class="p-3">
                                 <a class="btn btn-primary font-weight-bold" href="{{route('referral.index')}}">Посмотреть моих ожидающих
-                                    покупки подписчиков <i class="fa fa-arrow-right" aria-hidden="true"></i>
+                                    покупки подписчиков <span class="material-icons">search</span>
 
                                 </a>
                             </p>
